4e256a435cacb46358ca9a924569151d3767b116
Jason Hanna <jason.hanna01@gmail.com>
2016-12-18 20:29:03 -0700

    Completed Difference of Squares

---
+++ exercism/ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,25 @@
+class Squares
+  attr_reader :num
+
+  def initialize(num)
+    @num = (0..num).to_a
+  end
+
+  def square_of_sum(number = num)
+    num.reduce(:+)**2
+  end
+
+  def sum_of_squares(number = num)
+    squares = num.map {|number| number**2}
+    squares.reduce(:+)
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
+end
+
+module BookKeeping
+  VERSION = 3
+end
