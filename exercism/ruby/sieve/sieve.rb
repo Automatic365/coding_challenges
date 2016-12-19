bd4dde1756c22bcfef625e416198dc524c48f5f1
Jason Hanna <jason.hanna01@gmail.com>
2016-12-19 12:43:53 -0700

    all tests passing

---
+++ exercism/ruby/sieve/sieve.rb
@@ -0,0 +1,26 @@
+class Sieve
+  attr_reader :num_array, :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def primes
+    num_array = (2..limit).to_a
+    mult_array = (2..limit).to_a
+    num_array.each do |num|
+      mult_array.each do |mult|
+        result = num * mult
+        if num_array.include?(result)
+          num_array.delete(result)
+        end
+      end
+    end
+    num_array
+  end
+
+end
+
+module BookKeeping
+  VERSION = 1
+end
